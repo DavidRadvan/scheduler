08367430154f2d36360075b37c1e1e5234ce778f
David Radvan <david.radvan@gmail.com>
2021-04-12 22:32:37 -0700

    add Daylist components to Application

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -6,7 +6,7 @@ export default function DayList(props) {
 
   const dayArr = props.days.map(day => {
   return (<DayListItem
-  key={day.key}
+  key={day.id}
   name={day.name}
   spots={day.spots}
   selected={day.name === props.day}
