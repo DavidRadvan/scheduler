05899d5b9a7f94decc518ad4e100b506a3eab0a3
David Radvan <david.radvan@gmail.com>
2021-04-13 18:56:03 -0700

    add components confirm, empty, error, header, show, status

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "components/Appointment/styles.scss";
+import Header from "components/Appointment/Header";
 const classNames = require('classnames');
 
 export default function Appointment(props) {
