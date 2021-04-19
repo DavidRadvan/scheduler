a0f51a32b26dc267dee77a7da4d58628034714d2
David Radvan <david.radvan@gmail.com>
2021-04-19 15:49:27 -0700

    add appointment and form tests

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,9 +1,10 @@
 import React from "react";
+import PropTypes from 'prop-types'
 
 import InterviewerListItem from "components/InterviewerListItem";
 import "components/InterviewerList.scss";
 
-export default function InterviewerList(props) {
+function InterviewerList(props) {
 
   const interviewerArr = props.interviewers.map(interviewer => {
   return (<InterviewerListItem
@@ -22,3 +23,9 @@ export default function InterviewerList(props) {
   </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
+export default InterviewerList;
